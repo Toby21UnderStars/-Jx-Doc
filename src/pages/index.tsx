f6483da8b138b6a7803f924acda0a71d86118610
Jingxin Xu <x21days@qq.com>
2023-06-16 09:15:40 +0800

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      description="Jx Doc"> 
+      <main>
+        
+      </main>
+    </Layout>
+  );
+}
